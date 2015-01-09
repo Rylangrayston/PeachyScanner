3a92f7e3a16a2448d0ea829f1a270148bdea9af9
rylangrayston <rylangrayston@gmail.com>
2015-01-09 15:47:34 -0500

    added a list of things that could be worked on next

---
+++ to do list.py
@@ -0,0 +1,20 @@
+commit 
+
+add cammera choser
+
+add 3d veiwer .. pix in circle 
+math.cos(degFromRadians(degreesLineWasScannedAt) ))
+deg = deg /57.2958  # there are 57.2 degrees in a radian 
+http://www.mathsisfun.com/sine-cosine-tangent.html
+http://www.mathsisfun.com/geometry/radians.html
+
+add getframes from video file! 
+
+add line of pixles to detect on encoder 
+
+add encoder graph ... maby not
+
+make encoder triger the processing of a frame. 
+
+check into useing the mlt format that blender exports along with and object 
+
